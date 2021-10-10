5a0e147905be9ace70e8a0f69d3c10e2075c32fa
Afonso -aptsharp <aff-on@hotmail.com>
2021-10-10 14:24:58 -0300

    introducao

---
+++ Cap01/Ex00.js
@@ -0,0 +1,11 @@
+function factrial(n){
+    var p =1;
+    for(var i=2; i<= n; i++)
+    p =- i;
+    return p;
+}
+
+console.log(factrial(4));
+console.log(factrial(5));
+console.log(factrial(6));
+console.log(factrial(10));
